adcd94455db0b2d8913a31ee6a29c83f84ffff98
Matte Lim <17747414+mattelim@users.noreply.github.com>
2023-04-25 22:44:09 +0800

    Things work, separately

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ animal: textInput }),
+          body: JSON.stringify({ prompt: textInput }),
         });
 
         const data = await response.json();
@@ -29,8 +29,9 @@ export default function Home() {
           throw data.error || new Error(`Request failed with status ${response.status}`);
         }
 
-        setResult(data.result);
-        setTextInput("");
+        // setResult(data.result);
+        result = data.result;
+        // setTextInput("");
       } catch(error) {
         // Consider implementing your own error handling logic here
         console.error(error);
@@ -58,11 +59,12 @@ export default function Home() {
     const onChange = useCallback((value, viewUpdate) => {
       // console.log('value:', value);
       result = value;
+      // setResult(value)
       console.log('result:', result)
     }, []);
     return (
       <div className="overflow-scroll rounded border border-gray-100 shadow-md bg-white">
-        <CodeMirror
+        <CodeMirror key='code-mirror-01'
           value="// type a text prompt above and click 'Generate p5 code'"
           height="100%"
           width="100%"
@@ -75,138 +77,125 @@ export default function Home() {
   }
 
   function RunContainer() {
-    const [evalResult, setEvalResult] = useState("");
+    // const [evalResult, setEvalResult] = useState("");
     const [sandboxRunning, setSandboxRunning] = useState(false);
     // const sandboxDom = document.getElementById("sandbox");
+    // console.log(sandboxRunning);
 
-    async function onClick(event) {
+    function clickPlay(event) {
       // console.log("Hi dad")
       event.preventDefault();
-      try {
-        // // const result = await sandboxedEval(JSON.stringify(src));
-        // // const runLog = await sandboxedEval(result, setEvalResult, sandboxDom);
-        // const runLog = await sandboxedEval(result, setEvalResult);
-        // // evalResult.value = JSON.stringify(result);
-        // console.log(runLog);
-        setSandboxRunning(true);
-      } catch (e) {
-        // evalResult.value = e;
-      }
+      setSandboxRunning(true);
+      // try {
+      //   // // const result = await sandboxedEval(JSON.stringify(src));
+      //   // // const runLog = await sandboxedEval(result, setEvalResult, sandboxDom);
+      //   // const runLog = await sandboxedEval(result, setEvalResult);
+      //   // // evalResult.value = JSON.stringify(result);
+      //   // console.log(runLog);
+      //   setSandboxRunning(true);
+      // } catch (e) {
+      //   // evalResult.value = e;
+      // }
     }
 
-    function SandBox(running) {
-      let hrefOrigin = useRef()
-      
-      useEffect( () => {  
-          hrefOrigin.current= window.location.origin
-      }, []);
+    function clickStop(event) {
+      // console.log("Hi dad")
+      event.preventDefault();
+      setSandboxRunning(false);
+      // try {
+      //   // // const result = await sandboxedEval(JSON.stringify(src));
+      //   // // const runLog = await sandboxedEval(result, setEvalResult, sandboxDom);
+      //   // const runLog = await sandboxedEval(result, setEvalResult);
+      //   // // evalResult.value = JSON.stringify(result);
+      //   // console.log(runLog);
+      //   setSandboxRunning(false);
+      // } catch (e) {
+      //   // evalResult.value = e;
+      // }
+    }
 
-      const srcdoc = (origin, senderId, receiverId, src) =>
+    function SandBox({running}) {
+
+      const srcdoc = (src) =>
       `<!doctype html>
       <html>
-      <head>
-      <script src="https://cdn.jsdelivr.net/npm/p5@1.6.0/lib/p5.js"></script>
-      </head>
-      <body>
-      <main></main>
-      <script>
-      // delete window.fetch;
-      // delete window.XMLHttpRequest;
-      const origin = "${origin}";
-      const senderId = "${senderId}";
-      const receiverId = "${receiverId}";
+        <head>
+          <script src="https://cdn.jsdelivr.net/npm/p5@1.6.0/lib/p5.js"></script>
+        </head>
+        <body>
+          <main></main>
+          <!-- <textarea id="console" readonly class="block w-full bg-gray-100 rounded font-mono p-2 text-sm min-h-[70px] text-gray-500"></textarea> -->
+        </body>
+        <script>
+          delete window.fetch;
+          delete window.XMLHttpRequest;
 
-      // console.log = function(result) {
-      //     // document.getElementById('console').innerHTML = result;
-      //     window.parent.postMessage({ id: senderId, result }, origin);
-      // };
-      // console.log('your result');
-      // console.log('your result');
-      // console.log('your result');
+          console.log = function(logMsg) {
+            window.parent.postMessage(JSON.stringify({ logMsg }), '*');
+          };
 
-      // const handleMessage = (event) => {
-      //   if (event.source !== window.parent) {
-      //     return;
-      //   }
-      //   if (event.origin !== origin) {
-      //     return;
-      //   }
-      //   const { id, src, scope } = event.data || {};
-      //   if (id !== receiverId) {
-      //     return;
-      //   }
-      //   try {
-      //     const result =
-      //       new Function(...Object.keys(scope), '"use strict";' + src)(...Object.values(scope));
-      //     window.parent.postMessage({ id: senderId, result }, origin);
-      //   } catch (error) {
-      //     window.parent.postMessage({ id: senderId, error }, origin);
-      //   }
-      //   window.removeEventListener("message", handleMessage);
-      // };
-      // window.addEventListener("message", handleMessage);
-      // window.parent.postMessage({ id: senderId, ready: true }, origin);
-
-      // new Function('"use strict";' + {src})();
-      ${src}
-
-      let x = 100;
-      let y = 100;
-
-      function setup() {
-        createCanvas(200,200);
-        frameRate(30);
-      }
-
-      function draw() {
-        background(0);
-        fill(255);
-        ellipse(x,y,Math.random()*50,returnVal());
-        console.log(Math.random());
-        console.log(frameCount);
-        console.log(frameRate());
-      }
-
-      function returnVal() {
-          return 50;
-      }
-
-      </script>
-      </body>
+          ${src}
+        </script>
       </html>`;
-
+      
       if (!running) {
-        return null
+        return <div className="w-full min-h-[500px] bg-gray-100 rounded text-sm text-gray-400 flex justify-center items-center">No sketches running</div>
       }
-      return <iframe srcDoc={srcdoc(hrefOrigin.current, "sender", "receiver", result)} sandbox="allow-scripts" />;
+      return (
+        <div className="w-full min-h-[500px]">
+          <iframe width="100%" height="500" srcDoc={srcdoc(result)} sandbox="allow-scripts" />
+        </div>
+      );
+    }
+
+    function LogFauxBox() {
+      const [logMsg, setlogMsg] = useState("");
+
+      useEffect(() => {
+        let ranOnce = false;
+
+        const handler = event => {
+          const data = JSON.parse(event.data)
+          // console.log("Hello World?", data.logMsg)
+          if (!ranOnce) {
+            setlogMsg(data.logMsg);
+            ranOnce = true;
+          } else {
+            setlogMsg(msg => msg + '\n' + data.logMsg);
+          }
+        }
+    
+        window.addEventListener("message", handler)
+    
+        // clean up
+        return () => window.removeEventListener("message", handler)
+      }, [])
+
+      return <textarea className="block w-full bg-gray-100 rounded font-mono p-2 text-sm min-h-[70px] text-gray-500"
+        value={logMsg} readOnly/>
     }
 
     return (
       <div className="rounded border border-gray-100 shadow-md bg-white p-3 flex flex-col gap-2">
         <div className="flex gap-2">
-          <button 
-          className="rounded-full bg-emerald-500 text-white p-2 text-sm"
-          onClick={onClick}
-          >
+          <button className="rounded-full bg-emerald-500 text-white p-2 text-sm"
+          onClick={clickPlay}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-white stroke-[2]">
               <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
             </svg>
           </button>
-          <button className="rounded-full bg-emerald-500 text-white p-2 text-sm">
+          <button className="rounded-full bg-emerald-500 text-white p-2 text-sm"
+          onClick={clickStop}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-white stroke-[2]">
               <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
             </svg>
           </button>
         </div>
 
-        <textarea 
-          className="block w-full bg-gray-100 rounded font-mono p-2 text-sm min-h-[70px] text-gray-500"
-          value={evalResult} 
-          readOnly
-        />
-
         {/* <div id="sandbox">The canvas will go here</div> */}
         <SandBox running={sandboxRunning}/>
+
+        <LogFauxBox />
       </div>
     );
   }
@@ -219,7 +208,7 @@ export default function Home() {
       </Head>
       <div className="w-full p-5 flex flex-col gap-5">
         <TextInput />
-        <Editor />
+        <Editor key="editor-01"/>
         <RunContainer />
       </div>
     </>
