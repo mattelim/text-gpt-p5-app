c0ffff3eb1ec994509229728f7d7031380699c6b
Matte Lim <17747414+mattelim@users.noreply.github.com>
2023-04-27 11:15:53 +0800

    Added next analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
+ 
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
